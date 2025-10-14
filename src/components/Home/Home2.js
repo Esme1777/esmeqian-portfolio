e156365f7b5692b9c1832e86bc2105a7c04555f7
Esme Qian <qianyuxin2025@163.com>
2025-10-13 19:39:34 -0700

    update: home/about, email popup, avatar, projects covers

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,108 +1,99 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.svg";
+import myImg from "../../Assets/portrait.png";
 import Tilt from "react-parallax-tilt";
-import {
-  AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
-} from "react-icons/ai";
+import { AiFillGithub, AiOutlineMail } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Home2() {
+  const handleEmailClick = () => {
+    window.alert("Email: qianyuxin2025@163.com");
+  };
+
   return (
     <Container fluid className="home-about-section" id="about">
       <Container>
         <Row>
           <Col md={8} className="home-about-description">
             <h1 style={{ fontSize: "2.6em" }}>
-              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+              A LITTLE <span className="purple"> ABOUT </span> ME
             </h1>
+
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
-              <br />
-              <br />I am fluent in classics like
-              <i>
-                <b className="purple"> C++, Javascript and Go. </b>
-              </i>
+              I’m <span className="purple">Esme Qian</span>, an
+              <b> empathetic UX designer</b> exploring how{" "}
+              <b className="purple">human emotions</b> shape meaningful
+              interactions.
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Blockchain.
-                </b>
-              </i>
+              I focus on <b className="purple">human-centered research</b>,
+              translating insights into calm, intuitive flows that feel natural.
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
-              <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i>
+              I love <b className="purple">storytelling</b>—turning complex
+              systems into narratives people can understand and feel.
+              <br />
+              <br />
+              And I care about <b className="purple">aesthetic, creative design</b>,
+              where nature meets technology to create resonant experiences.
             </p>
           </Col>
+
           <Col md={4} className="myAvtar">
             <Tilt>
-              <img src={myImg} className="img-fluid" alt="avatar" />
+              <img
+  src={myImg}
+  className="img-fluid"
+  alt="Esme Qian portrait"
+  style={{ width: 320, height: 320, borderRadius: "50%", objectFit: "cover" }}
+/>
             </Tilt>
           </Col>
         </Row>
+
         <Row>
           <Col md={12} className="home-about-social">
             <h1>FIND ME ON</h1>
             <p>
-              Feel free to <span className="purple">connect </span>with me
+              Feel free to <span className="purple">connect</span> with me
             </p>
+
             <ul className="home-about-social-links">
               <li className="social-icons">
+                <button
+                  onClick={handleEmailClick}
+                  className="icon-colour home-social-icons"
+                  aria-label="Show Email"
+                  title="Show Email"
+                  style={{ background: "transparent", border: "none", cursor: "pointer" }}
+                >
+                  <AiOutlineMail />
+                </button>
+              </li>
+
+              <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
+                  href="https://github.com/Esme1777"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  className="icon-colour home-social-icons"
+                  aria-label="GitHub"
+                  title="GitHub"
                 >
                   <AiFillGithub />
                 </a>
               </li>
+
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/Soumyajit4419"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiOutlineTwitter />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <FaLinkedinIn />
-                </a>
-              </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.instagram.com/soumyajit4419"
+                  href="https://www.linkedin.com/in/esme-qian-947136386"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
+                  aria-label="LinkedIn"
+                  title="LinkedIn"
                 >
-                  <AiFillInstagram />
+                  <FaLinkedinIn />
                 </a>
               </li>
             </ul>
@@ -112,4 +103,5 @@ function Home2() {
     </Container>
   );
 }
+
 export default Home2;
